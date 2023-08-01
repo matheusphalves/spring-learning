95e71d2bea064d57f5a30e432af62edb1b7d6390
Matheus Phelipe <matheusphalves@gmail.com>
2023-08-01 11:51:31 -0300

    start creating unit tests

---
+++ src/test/java/com/learning/spring/demo/api/controller/ProductControllerTest.java
@@ -0,0 +1,4 @@
+package com.learning.spring.demo.api.controller;
+
+public class ProductControllerTest {
+}
---
+++ src/test/java/com/learning/spring/demo/domain/service/ProductServiceImplTest.java
@@ -0,0 +1,39 @@
+package com.learning.spring.demo.domain.service;
+
+import com.learning.spring.demo.domain.model.ProductModel;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@SpringBootTest
+class ProductServiceImplTest {
+
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    void saveProductShouldReturnCreatedProductModel() {
+
+        String productName = "Product Test";
+        BigDecimal productValue = new BigDecimal("12.0");
+
+        ProductModel productModel = new ProductModel();
+        productModel.setName(productName);
+        productModel.setValue(productValue);
+
+        ProductModel createdProductModel = productService.saveProduct(productModel);
+
+        assertNotNull(createdProductModel.getIdProduct());
+        assertNotNull(createdProductModel.getCreatedAt());
+        assertNull(createdProductModel.getUpdatedAt());
+        assertEquals(productName, createdProductModel.getName());
+        assertEquals(productValue, createdProductModel.getValue());
+
+    }
+}
