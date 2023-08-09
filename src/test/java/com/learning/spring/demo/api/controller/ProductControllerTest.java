326e69904123fd8f23b82395093ca15ed701490b
Matheus Phelipe <matheusphalves@gmail.com>
2023-08-09 17:00:39 -0300

    create tests for controller component

--- src/test/java/com/learning/spring/demo/api/controller/ProductControllerTest.java
+++ src/test/java/com/learning/spring/demo/api/controller/ProductControllerTest.java
@@ -1,5 +1,6 @@
 package com.learning.spring.demo.api.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.learning.spring.demo.api.model.ProductRecordDto;
 import com.learning.spring.demo.domain.model.ProductModel;
 import com.learning.spring.demo.domain.service.ProductService;
@@ -9,13 +10,19 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @ExtendWith(MockitoExtension.class)
 class ProductControllerTest {
@@ -25,31 +32,50 @@ class ProductControllerTest {
     @Mock
     private ProductService productService;
 
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper;
+
     private ProductRecordDto productRecordDto;
 
     @BeforeEach
     void setup() {
 
+        mockMvc = MockMvcBuilders.standaloneSetup(productController)
+                .alwaysDo(print())
+                .build();
+
+        objectMapper = new ObjectMapper();
+
         productRecordDto = new ProductRecordDto(
                 "name",
                 new BigDecimal("12.0"),
                 new BigDecimal("0.5"),
-                LocalDateTime.now(),
-                LocalDateTime.now()
+                null,null
         );
 
     }
 
     @Test
-    void shouldSaveProduct() {
+    void shouldSaveProduct() throws Exception {
 
-        ProductModel productModel = new ProductModel();
-        productModel.setName("name");
+        when(productService.saveProduct(any(ProductModel.class)))
+                .thenReturn(any(ProductModel.class));
 
-        when(productService.saveProduct(any(ProductModel.class))).thenReturn(productModel);
+        mockMvc.perform(post("/products")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(productRecordDto)))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andReturn();
 
-        ResponseEntity<ProductModel> response =
-                productController.saveProduct(productRecordDto);
+        verify(productService).saveProduct(any(ProductModel.class));
+    }
+
+    @Test
+    void shouldReturnListOfProducts() throws Exception{
+        mockMvc.perform(get("/products"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
 }
