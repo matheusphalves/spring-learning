25cae7461e8f631d1551e35735ed9bbacb64a5d7
Matheus Phelipe <matheusphalves@gmail.com>
2023-08-09 15:40:51 -0300

    refactor texts to run with mockito

--- src/test/java/com/learning/spring/demo/domain/service/ProductServiceImplTest.java
+++ src/test/java/com/learning/spring/demo/domain/service/ProductServiceImplTest.java
@@ -1,41 +1,97 @@
 package com.learning.spring.demo.domain.service;
 
+import com.learning.spring.demo.domain.exception.NoSuchProductFoundException;
 import com.learning.spring.demo.domain.model.ProductModel;
+import com.learning.spring.demo.domain.repository.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class ProductServiceImplTest {
 
 
-    @Autowired
-    private ProductService productService;
+    @InjectMocks
+    private ProductServiceImpl productServiceImpl;
+
+    @Mock
+    private ProductRepository productRepository;
+
+    private ProductModel productModel;
+
+    @BeforeEach
+    public void setUp(){
+        productModel = new ProductModel();
+        productModel.setIdProduct(UUID.randomUUID());
+        productModel.setName("Name");
+        productModel.setValue(new BigDecimal("12.0"));
+        productModel.setDiscountRate(2L);
+    }
 
     @Test
     void saveProductShouldReturnCreatedProductModel() {
 
-        String productName = "Product Test";
-        BigDecimal productValue = new BigDecimal("12.0");
+        when(productRepository.save(productModel)).thenReturn(productModel);
 
-        ProductModel productModel = new ProductModel();
-        productModel.setName(productName);
-        productModel.setValue(productValue);
-
-        ProductModel createdProductModel = productService.saveProduct(productModel);
+        ProductModel createdProductModel = productServiceImpl.saveProduct(productModel);
 
         assertNotNull(createdProductModel.getIdProduct());
         assertNotNull(createdProductModel.getCreatedAt());
         assertNull(createdProductModel.getUpdatedAt());
-        assertEquals(productName, createdProductModel.getName());
-        assertEquals(productValue, createdProductModel.getValue());
+        assertEquals("Name", createdProductModel.getName());
+        assertEquals(new BigDecimal("12.0"), createdProductModel.getValue());
+
+    }
+
+    @Test
+    void findProductByIdShouldThrowsNoSuchProductFoundExceptionWhenProductIsNotFound(){
+
+        UUID id = UUID.randomUUID();
+
+        when(productRepository.findById(id))
+                .thenReturn(Optional.empty());
+
+        assertThrows(NoSuchProductFoundException.class,
+                () -> productServiceImpl.findProductById(id));
+
+    }
+
+    @Test
+    void findAllProductsShouldReturnListOfProductModel(){
+        when(productRepository.findAll())
+                .thenReturn(Collections.singletonList(productModel));
+
+        List<ProductModel> productModels = productServiceImpl.findAllProducts();
+
+        assertEquals(1, productModels.size());
+
+    }
+
+    @Test
+    void deleteProductShouldDeleteExistingProductModel(){
+        UUID id = UUID.randomUUID();
+
+        when(productRepository.findById(id)).thenReturn(Optional.of(productModel));
+
+        productServiceImpl.deleteProduct(id);
+
+        verify(productRepository).deleteById(id);
 
     }
 }
